bfcac6982e4639d4a6cbd612f5b6c86d2c24f3f5
AraceliGS <aragutarras@gmail.com>
2018-01-25 16:15:49 -0500

    modificando la funcionalidad de este proyecto

--- js/app.js
+++ js/app.js
@@ -1,60 +1,74 @@
-// Creando función cipher
+$(document).ready(function() {
+  // Creando función cipher
 
-function cipher(str) {
-  var letters = []; // ['H', 'I', 'J'] || ['h', 'i', 'j']
-  var lettersArray = str.split('');
-  for (var i = 0; i < lettersArray.length; i++) {
-    if (lettersArray[i] === lettersArray[i].toUpperCase()) {
-      var letterAscii = str.charCodeAt(i);
-      var ciphering = (letterAscii - 65 + 33) % 26 + 65;
-      var alphabetLetters1 = String.fromCharCode(ciphering);
-      letters.push(alphabetLetters1);
+  function cipher(str, array) {
+    var letters = []; // ['H', 'I', 'J'] || ['h', 'i', 'j']
+    var lettersArray = array;
+    for (var i = 0; i < lettersArray.length; i++) {
+      if (lettersArray[i] === lettersArray[i].toUpperCase()) {
+        var letterAscii = str.charCodeAt(i);
+        var ciphering = (letterAscii - 65 + 33) % 26 + 65;
+        var alphabetLetters1 = String.fromCharCode(ciphering);
+        letters.push(alphabetLetters1);
+      }
+      if (lettersArray[i] === lettersArray[i].toLowerCase()) {
+        var letterAscii2 = str.charCodeAt(i);
+        var ciphering2 = (letterAscii2 - 97 + 33) % 26 + 97;
+        var alphabetLetters2 = String.fromCharCode(ciphering2);
+        letters.push(alphabetLetters2);
+      }
     }
-    if (lettersArray[i] === lettersArray[i].toLowerCase()) {
-      var letterAscii2 = str.charCodeAt(i);
-      var ciphering2 = (letterAscii2 - 97 + 33) % 26 + 97;
-      var alphabetLetters2 = String.fromCharCode(ciphering2);
-      letters.push(alphabetLetters2);
-    }
-  }
-  var arrayToString1 = letters.join('');
-  return arrayToString1;
-}
-
-// Creando función decipher
-
-function decipher(str) {
-  var letters2 = []; // ['A', 'B', 'C'] || ['a', 'b' ,'c']
-  var lettersArray2 = str.split('');
-  for (var i = 0; i < lettersArray2.length; i++) {
-    if (lettersArray2[i] === lettersArray2[i].toUpperCase()) {
-      var letterAscii3 = str.charCodeAt(i);
-      var deciphering = (letterAscii3 + 65 - 33) % 26 + 65;
-      var alphabetLetters3 = String.fromCharCode(deciphering);
-      letters2.push(alphabetLetters3);
-    }
-    if (lettersArray2[i] === lettersArray2[i].toLowerCase()) {
-      var letterAscii4 = str.charCodeAt(i);
-      var deciphering2 = (letterAscii4 - 97 + 33) % 26 + 83;
-      var alphabetLetters4 = String.fromCharCode(deciphering2);
-      letters2.push(alphabetLetters4);
-    }
-   }
-   var arrayToString2 = letters2.join('');
-   return arrayToString2; 
+    var arrayToString1 = letters.join('');
+    return arrayToString1;
   }
 
-// Preguntando al usuario
+  // Creando función decipher
 
-var phrase = prompt('Ingrese una frase');
-
-if (typeof(phrase) === 'string' && phrase !== '') {
-  var options = '1. Quieres cifrar esta frase según el código ASCII \n 2.Quieres descifrar esta frase en código ASCII.\n';
-  var op = parseInt(prompt(options));
-
-  if (op === 1) {
-    console.log(cipher(phrase));
-  } else {
-    console.log(decipher(phrase));
+  function decipher(str, array) {
+    var letters2 = []; // ['A', 'B', 'C'] || ['a', 'b' ,'c']
+    var lettersArray2 = array;
+    for (var i = 0; i < lettersArray2.length; i++) {
+      if (lettersArray2[i] === lettersArray2[i].toUpperCase()) {
+        var letterAscii3 = str.charCodeAt(i);
+        var deciphering = (letterAscii3 + 65 - 33) % 26 + 65;
+        var alphabetLetters3 = String.fromCharCode(deciphering);
+        letters2.push(alphabetLetters3);
+      }
+      if (lettersArray2[i] === lettersArray2[i].toLowerCase()) {
+        var letterAscii4 = str.charCodeAt(i);
+        var PATTERNLETTERS = /[^h-s]/g;
+        if (lettersArray2[i].match(PATTERNLETTERS)) {
+          var deciphering2 = (letterAscii4 - 97 + 33) % 26 + 109;
+        } else {
+          var deciphering2 = (letterAscii4 - 97 + 33) % 26 + 83;
+        }
+        var alphabetLetters4 = String.fromCharCode(deciphering2);
+        letters2.push(alphabetLetters4);
+      }
+    }
+    var arrayToString2 = letters2.join('');
+    return arrayToString2; 
   }
-}
+
+  // Preguntando al usuario
+
+  $('#button').on('click', function() {
+    var word = $('#inputText').val().trim();
+    var PATTERNSTRING = /[^0-9]/g;
+    var result = word.match(PATTERNSTRING);
+    var string = result.join('');
+    console.log(result);
+    // Preguntando al usuario
+    if (result) {
+      var options = '1. Quieres cifrar esta frase según el código ASCII \n 2.Quieres descifrar esta frase en código ASCII.\n';
+      var op = parseInt(prompt(options));
+  
+      if (op === 1) {
+        alert(cipher(string, result));
+      } else {
+        alert(decipher(string, result));
+      }
+    }
+  });
+});
+
